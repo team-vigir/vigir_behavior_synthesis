476921905f43f6896c0928827d8e43cfc28fec6e
Spyros Maniatopoulos <sm2296@cornell.edu>
2015-03-20 10:53:25 -0400

    Fix ltl_compilation after msg/srv renaming

--- vigir_bs_manager/src/vigir_bs_manager/ltl_compilation_client.py
+++ vigir_bs_manager/src/vigir_bs_manager/ltl_compilation_client.py
@@ -2,7 +2,7 @@
 
 import rospy
 
-from vigir_bs_msgs.srv import CompileLTL
+from vigir_bs_msgs.srv import LTLCompilation
 from vigir_bs_msgs.msg import LTLFormula, BSErrorCodes
 
 def ltl_compilation_client():
@@ -11,13 +11,13 @@ def ltl_compilation_client():
     rospy.wait_for_service('ltl_compilation')
     
     try:
-        ltl_compulation_srv = rospy.ServiceProxy('ltl_compilation', CompileLTL)
+        ltl_compulation_srv = rospy.ServiceProxy('ltl_compilation', LTLCompilation)
         resp = ltl_compulation_srv(['stand'], ['manipulate'])
         
         #DEBUG
-        print resp.error_code.value
-        print resp.ltl_formula.sys_init
-        print resp.ltl_formula.sys_liveness
+        # print resp.error_code.value
+        # print resp.ltl_formula.sys_init
+        # print resp.ltl_formula.sys_liveness
         
         return resp.ltl_formula
     
--- vigir_bs_specification/src/vigir_bs_specification/ltl_compilation_server.py
+++ vigir_bs_specification/src/vigir_bs_specification/ltl_compilation_server.py
@@ -2,14 +2,14 @@
 
 import rospy
 
-from vigir_bs_msgs.srv import *
+from vigir_bs_msgs.srv import LTLCompilation
 from vigir_bs_msgs.msg import LTLFormula, BSErrorCodes
 
-def handle_ltl_compilation(req):
+def handle_ltl_compilation(request):
     '''Responsible for putting together a complete LTL specification.'''
     
-    initial_conditions = req.initial_conditions
-    goal = req.goal
+    initial_conditions = request.initial_conditions
+    goal = request.goal
 
     ltl_specification = LTLFormula()
     # temp
@@ -19,14 +19,14 @@ def handle_ltl_compilation(req):
     # Behavior Synthesis error code
     error_code = BSErrorCodes(BSErrorCodes.SUCCESS)
 
-    return CompileLTLResponse(error_code, ltl_specification)
+    return LTLCompilationResponse(ltl_specification, error_code)
 
 def ltl_compilation_server():
     ''''Server'''
     
     rospy.init_node('ltl_compilation_server')
     
-    s = rospy.Service('ltl_compilation', CompileLTL, handle_ltl_compilation)
+    s = rospy.Service('ltl_compilation', LTLCompilation, handle_ltl_compilation)
     
     rospy.loginfo("Ready to receive LTL Specification Compilation requests.")
     rospy.spin()
