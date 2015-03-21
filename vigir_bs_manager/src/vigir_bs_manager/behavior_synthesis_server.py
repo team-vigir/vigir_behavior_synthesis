ce2f2b46b15b712e86a9b35da23804ca2c08a5e1
Spyros Maniatopoulos <sm2296@cornell.edu>
2015-03-21 12:43:51 -0400

    [vigir_bs_manager] BS Action server
    * Add main logic
    * Add placeholder methods for the 3 requests
    * Add helper method for pub feedback

--- vigir_bs_manager/src/vigir_bs_manager/behavior_synthesis_server.py
+++ vigir_bs_manager/src/vigir_bs_manager/behavior_synthesis_server.py
@@ -5,12 +5,14 @@ import roslib; roslib.load_manifest('vigir_bs_manager')
 import rospy
 import actionlib
 
-from vigir_bs_msgs.msg import *
+from vigir_bs_msgs.msg import BehaviorSynthesisAction, BehaviorSynthesisFeedback, BehaviorSynthesisResult, BSErrorCodes
 
 import ltl_compilation_client
 
 class BehaviorSynthesisActionServer(object):
-    # create messages that are used to publish feedback/result
+    '''...'''
+
+    # Messages that are used to publish feedback/result
     _feedback = BehaviorSynthesisFeedback()
     _result   = BehaviorSynthesisResult()
 
@@ -20,33 +22,80 @@ class BehaviorSynthesisActionServer(object):
         self._as.start()
 
     def execute_cb(self, goal):
-        # helper variables
-        r = rospy.Rate(1)
-        success = True
+        '''...'''
 
-        # append the seeds for the fibonacci sequence
-        self._feedback.status = "Executing behavior synthesis"
+        r = rospy.Rate(1)   #FIX: What should this be?
+        success = True      # start optimistically
 
-        # start executing the action
-        # check that preempt has not been requested by the client
+        # Acknowledge goal reception
+        self.set_and_publish_feedback("Received behavior synthesis request.")
+
+        # Examine the goal message
+        bs_goal = goal.request
+        bs_options = goal.synthesis_options
+
         #TODO: receive a callback when a preempt request is received
         if self._as.is_preempt_requested():
             rospy.loginfo('%s: Preempted' % self._action_name)
             self._as.set_preempted()
             success = False
 
-        # Request an LTL Specification from the corresponding server
-        ltl_compilation_client.ltl_compilation_client()
-        self._feedback.status = "Received LTL Specification"
+        if success:  
+            # Request LTL Specification Compilation from the corresponding server
+            # and also update and publish the appropriate feedback
+            ltl_spec, error_code_value, success = self.handle_ltl_specification_request()
 
-        # publish the feedback
-        self._as.publish_feedback(self._feedback)
-          
+        if success: pass
+            # Request LTL Synthesis from the corresponding server
+            # and also update and publish the appropriate feedback
+            # automaton, error_code_value, success = self.handle_ltl_synthesis_request()
+
+        if success: pass
+            # Request State Machine Generation from the corresponding server
+            # and also update and publish the appropriate feedback
+            # sm, error_code_value, success = self.handle_sm_generation_request()
+
         if success:
             self._result.error_code = BSErrorCodes(BSErrorCodes.SUCCESS)
+            # self._result.states = sm
             rospy.loginfo('%s: Succeeded' % self._action_name)
             self._as.set_succeeded(self._result)
-      
+        else:
+            self._result.error_code = BSErrorCodes(error_code_value)
+            rospy.logerr('%s: Failed' % self._action_name)
+            self._as.set_failed(self._result)
+    
+    def handle_ltl_specification_request(self):
+        '''...'''
+
+        response = ltl_compilation_client.ltl_compilation_client()
+        
+        # Update success and publish feedback based on response
+        if response.error_code.value is BSErrorCodes.SUCCESS:
+            self.set_and_publish_feedback("Received LTL specification")
+            success = True
+        else:
+            self.set_and_publish_feedback("Did not receive LTL specification")
+            success = False
+
+        return response.ltl_specification, response.error_code.value, success
+
+    def handle_ltl_synthesis_request(self):
+        '''...'''
+
+        pass
+
+    def handle_sm_generation_request(self):
+        '''...'''
+
+        pass
+
+    def set_and_publish_feedback(self, status):
+        '''Helper method for updating and publishing feedback.''' 
+        
+        self._feedback.status = status
+        self._as.publish_feedback(self._feedback)
+
 if __name__ == '__main__':
     rospy.init_node('behavior_synthesis')
     BehaviorSynthesisActionServer(rospy.get_name())
