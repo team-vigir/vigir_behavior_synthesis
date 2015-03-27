b3bbf6840a0d5ab8b9c5d1291120debe21481765
Spyros Maniatopoulos <sm2296@cornell.edu>
2015-03-27 12:57:06 -0400

    [vigir_synthesis_manager] Add LTL Synthesis client

---
+++ vigir_synthesis_manager/src/vigir_synthesis_manager/ltl_synthesis_client.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import rospy
+
+from vigir_synthesis_msgs.srv import LTLSynthesis
+
+def ltl_synthesis_client(ltl_spec, name):
+    '''Client'''
+
+    rospy.wait_for_service('ltl_synthesis')
+    
+    try:
+        ltl_synthesis_srv = rospy.ServiceProxy('ltl_synthesis', LTLSynthesis)
+        response = ltl_synthesis_srv(ltl_spec, name)
+        
+        #DEBUG
+        print response.automaton
+        print 'LTL Synthesis error code: ', response.error_code
+        
+        return response
+    
+    except rospy.ServiceException as e:
+        print("Service call failed: %s" % e)
+
+if __name__ == "__main__":
+    ltl_synthesis_client()
