b814370712f12d414579a3ff5fbed8e2808a23b1
Spyros Maniatopoulos <sm2296@cornell.edu>
2015-03-27 13:02:23 -0400

    [vigir_synthesis_manager] LTL Compilation client

--- vigir_synthesis_manager/src/vigir_synthesis_manager/ltl_compilation_client.py
+++ vigir_synthesis_manager/src/vigir_synthesis_manager/ltl_compilation_client.py
@@ -5,14 +5,14 @@ import rospy
 from vigir_synthesis_msgs.srv import LTLCompilation
 # from vigir_synthesis_msgs.msg import LTLSpecification, BSErrorCodes
 
-def ltl_compilation_client():
+def ltl_compilation_client(system, goals, initial_conditions, custom_ltl = None):
     '''Client'''
 
     rospy.wait_for_service('ltl_compilation')
     
     try:
-        ltl_compulation_srv = rospy.ServiceProxy('ltl_compilation', LTLCompilation)
-        response = ltl_compulation_srv(['stand'], ['manipulate'], 'atlas')
+        ltl_compilation_srv = rospy.ServiceProxy('ltl_compilation', LTLCompilation)
+        response = ltl_compilation_srv(system, goals, initial_conditions)
         
         #DEBUG
         print response.ltl_specification.sys_init
@@ -21,7 +21,7 @@ def ltl_compilation_client():
         print response.ltl_specification.env_trans
         print response.ltl_specification.sys_liveness
         print response.ltl_specification.env_liveness
-        print response.error_code
+        print 'LTL Compilation error code: ', response.error_code
         
         return response
     
@@ -29,4 +29,4 @@ def ltl_compilation_client():
         print("Service call failed: %s" % e)
 
 if __name__ == "__main__":
-    ltl_compilation_client()
+    ltl_compilation_client('atlas', ['manipulate'], ['stand'])
