2a6a7b6fff73e6622ac8a4cdc97fa2701225691d
Spyros Maniatopoulos <sm2296@cornell.edu>
2015-04-23 10:26:17 -0400

    [vigir_ltl_specification] Add module for task-specific specs

---
+++ vigir_ltl_specification/src/vigir_ltl_specification/task_specification.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import os
+import pprint
+
+import preconditions as precond
+from gr1_specification import GR1Specification
+from gr1_formulas import GR1Formula, FastSlowFormula
+
+"""
+Module's docstring #TODO
+"""
+
+VIGIR_ROOT_DIR = os.environ['VIGIR_ROOT_DIR']
+
+class TaskSpecification(GR1Specification):
+	"""..."""
+	
+	def __init__(self, spec_name = '', env_props = [], sys_props = []):
+		super(TaskSpecification, self).__init__(spec_name, env_props, sys_props)
+
+# =========================================================
+# Entry point
+# =========================================================
+
+def main():
+	
+	my_spec = TaskSpecification()
+
+	print 'Environment props:\t', my_spec.env_props
+	print 'System props:\t\t', my_spec.sys_props
+
+if __name__ == "__main__":
+	main()
+	
