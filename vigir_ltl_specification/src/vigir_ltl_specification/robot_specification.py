ba918022f3391d24a2432ec8e40271b0bde1cd35
Spyros Maniatopoulos <sm2296@cornell.edu>
2015-06-24 16:50:56 -0400

    [vigir_ltl_specification] Define RobotConfiguration and add unit tests

--- vigir_ltl_specification/src/vigir_ltl_specification/robot_specification.py
+++ vigir_ltl_specification/src/vigir_ltl_specification/robot_specification.py
@@ -1,5 +1,75 @@
 #!/usr/bin/env python
 
+import os
+import yaml
+
+from gr1_specification import GR1Specification
+from activation_outcomes import *
+
 """
 Module's docstring #TODO
+
+* Generate formulas
+* Load formulas in specification
+
 """
+
+class ActionSpecification(GR1Specification):
+    """
+    docstring for ActionSpecification
+
+    Arguments:
+      ts    dict    Dictionary encoding a transition system (TS).
+
+    """
+    def __init__(self, name = ''):
+        super(ActionSpecification, self).__init__(spec_name = name,
+                                                  env_props = [],
+                                                  sys_props = [])
+
+
+class RobotConfiguration(object):
+    """
+    docstring for RobotConfiguration
+
+    Arguments:
+      robot     string  The system / robot whose configuration will be loaded.
+
+    """
+    def __init__(self, robot):
+        self._robot = robot
+        
+        self._full_config = self._load_config_from_file(robot)
+        self.ts, self.preconditions = self._extract_configs()
+
+    @staticmethod
+    def _load_config_from_file(robot):
+        """..."""
+        
+        # Get absolute path to this module
+        module_path = os.path.dirname(__file__)
+        config_file = ('%s_config.yaml' % robot)
+        rel_config_path = 'config/' + config_file
+
+        config_file_path = os.path.join(module_path, rel_config_path)
+
+        try:
+            with open(config_file_path, 'r') as stream:
+                config = yaml.load(stream)
+        except IOError as e:
+            print('Failed to load {0}! {1}'.format(config_file, e))
+            config = dict()
+        
+        return config
+
+    def _extract_configs(self):
+        """Extract the individual elements of a robot configuration file."""
+
+        try:
+            ts = self._full_config['transition_system']
+            preconditions = self._full_config['action_preconditions']
+        except KeyError as e:
+            print('Failed to extract configuration element {}!'.format(e))
+            ts, preconditions = {}, {}
+
+        return (ts, preconditions)
--- vigir_ltl_specification/test/unit/robot_specification_test.py
+++ vigir_ltl_specification/test/unit/robot_specification_test.py
@@ -3,3 +3,71 @@
 import unittest
 
 from vigir_ltl_specification.robot_specification import *
+
+class SpecificationConstructionTests(unittest.TestCase):
+    """Test the generation of Activation-Outcomes formulas"""
+
+    def setUp(self):
+        """Gets called before every test case."""
+
+        self.spec_name = 'test'
+
+        self.spec = ActionSpecification(name = self.spec_name)
+
+        print("Setting up a new specification construction test.")
+
+    def tearDown(self):
+        """Gets called after every test case."""
+
+        print("Cleaning up after latest test ...")
+
+        del self.spec
+
+    def test_input_to_object(self):
+
+        self.assertEqual(self.spec_name, self.spec.spec_name)
+
+class ConfigurationTests(unittest.TestCase):
+    """Test the generation of Activation-Outcomes formulas"""
+
+    def setUp(self):
+        """Gets called before every test case."""
+
+        self.robot = 'atlas'
+
+        self.config = RobotConfiguration(robot = self.robot)
+
+        print("Setting up a new robot configuration test.")
+
+    def tearDown(self):
+        """Gets called after every test case."""
+
+        print("Cleaning up after latest test ...")
+
+        del self.config
+
+    def test_input_to_object(self):
+
+        self.assertEqual(self.robot, self.config._robot)
+
+    def test_load_config_file(self):
+    	
+    	self.assertIsInstance(self.config._full_config, dict)
+    	self.assertIsInstance(self.config.ts, dict)
+    	self.assertIsInstance(self.config.preconditions, dict)
+
+    def test_incorrect_input(self):
+    	
+    	self.config = RobotConfiguration(robot = 'bad_robot_name')
+
+    	self.assertItemsEqual(dict(), self.config._full_config)
+    	self.assertItemsEqual(dict(), self.config.ts)
+    	self.assertItemsEqual(dict(), self.config.preconditions)
+
+# =============================================================================
+# Entry point
+# =============================================================================
+
+if __name__ == '__main__':
+    # Run all tests
+    unittest.main()
