4d4056b70ac1c9f904010ecb88b97867a4465378
Spyros Maniatopoulos <sm2296@cornell.edu>
2015-06-24 13:48:11 -0400

    [vigir_ltl_specification] Define TransitionSystemSpecification

--- vigir_ltl_specification/src/vigir_ltl_specification/ts_specification.py
+++ vigir_ltl_specification/src/vigir_ltl_specification/ts_specification.py
@@ -1,8 +1,46 @@
 #!/usr/bin/env python
 
-import yaml
+from gr1_specification import GR1Specification
+from activation_outcomes import *
 
-# Inherit from GR1Specification
-# Load TS from .yaml file
-# Generate formulas
-# Load formulas in specification
+"""
+Module's docstring #TODO
+
+* Generate formulas
+* Load formulas in specification
+
+"""
+
+class TransitionSystemSpecification(GR1Specification):
+    """
+    docstring for TransitionSystemSpecification
+
+    Arguments:
+      ts    dict    Dictionary encoding a transition system (TS).
+
+    """
+    def __init__(self, name = '', ts = {}, props_of_interest = []):
+        super(TransitionSystemSpecification, self).__init__(spec_name = name,
+                                                            env_props = [],
+                                                            sys_props = [])
+        
+        self.ts = self._get_ts_of_interest(ts, props_of_interest)
+
+        #TODO: Also handle initial condition here [?] Maybe in other module ...
+
+        #TODO: Generate the necessary ActivationOutcomes formulas
+        #TODO: Load formulas into specification
+
+    @staticmethod
+    def _get_ts_of_interest(original_ts, props_of_interest):
+        """..."""
+
+        if not props_of_interest:
+            ts = original_ts
+        else:
+            ts = dict()
+            for pi in props_of_interest:
+                # Cherry-pick the props of interest from the dictionary
+                transitions = original_ts[pi]
+                ts[pi] = [t for t in transitions if t in props_of_interest]
+        return ts
