c60bfb0d3bde133f1e24ec2142f9284637bd8ba5
Spyros Maniatopoulos <sm2296@cornell.edu>
2015-06-25 11:11:56 -0400

    [vigir_ltl_specification][test] Update unit tests

--- vigir_ltl_specification/test/unit/activation_outcomes_test.py
+++ vigir_ltl_specification/test/unit/activation_outcomes_test.py
@@ -14,12 +14,8 @@ class FormulaGenerationTests(unittest.TestCase):
 		self.sys_props = ['dance', 'sleep']
 		self.outcomes  = ['completed', 'failed', 'preempted']
 
-		print("Setting up a new formula test.")
-
 	def tearDown(self):
 		"""Gets called after every test case."""
-
-		print("Cleaning up after latest test ...")
 
 		del self.sys_props
 		del self.outcomes
@@ -31,24 +27,38 @@ class FormulaGenerationTests(unittest.TestCase):
 
 		# Test whether the obvious things are working as expected
 		self.assertItemsEqual(self.outcomes, formula.outcomes)
-		self.assertItemsEqual(self.sys_props, formula.sys_props)
-		self.assertEqual(list(), formula.env_props)
 		self.assertEqual(list(), formula.formulas)
 
 		# Test whether the activation propositions are generated correctly
 		expected_act_props = ['sleep_a', 'dance_a']
-		self.assertItemsEqual(expected_act_props, formula.activation)
+		self.assertItemsEqual(expected_act_props, formula.sys_props)
 
 		# Test whether the outcome propositions are generated correctly
 		expected_out_props = {'dance': ['dance_c', 'dance_f', 'dance_p'],
 							  'sleep': ['sleep_c', 'sleep_f', 'sleep_p']}
 		self.assertItemsEqual(expected_out_props, formula.outcome_props)
 
-	def test_formulas_raise_exceptions(self):
+		# Test whether the environment propositions are generated correctly
+		expected_env_props = ['dance_c', 'dance_f', 'dance_p',
+							  'sleep_c', 'sleep_f', 'sleep_p']
+		self.assertItemsEqual(expected_env_props, formula.env_props)
+
+	def test_constructor_raises_exceptions(self):
 		"""Test whether the formula constructors raise Exceptions"""
 
 		self.assertRaises(TypeError, ActivationOutcomesFormula, ['dance', 1.0])
-		self.assertRaises(TypeError,ActivationOutcomesFormula, ['dance'],[2.0])
+		self.assertRaises(ValueError, ActivationOutcomesFormula, ['dance'],[])
+		self.assertRaises(TypeError, ActivationOutcomesFormula, ['dance'],[2.0])
+		self.assertRaises(ValueError, ActivationOutcomesFormula, ['dance'],
+						  ['completed', 'capitalized', 'called', 'calculated'])
+
+	def test_preconditions_formula(self):
+		
+		formula = PreconditionsFormula('a', ['a1', 'a2'])
+
+		expected_formula = '! a1 | ! a2 -> ! a'
+
+		self.assertEqual(expected_formula, formula.formulas[0])
 
 # =============================================================================
 # The following code is not actually useful. It will be removed promptly.
--- vigir_ltl_specification/test/unit/robot_specification_test.py
+++ vigir_ltl_specification/test/unit/robot_specification_test.py
@@ -5,30 +5,73 @@ import unittest
 from vigir_ltl_specification.robot_specification import *
 
 class SpecificationConstructionTests(unittest.TestCase):
-    """Test the generation of Activation-Outcomes formulas"""
+    """Test the construction of the ActionSpecification class."""
 
     def setUp(self):
         """Gets called before every test case."""
 
         self.spec_name = 'test'
+        self.preconditions = {'a': ['b']}
 
-        self.spec = ActionSpecification(name = self.spec_name)
-
-        print("Setting up a new specification construction test.")
+        self.spec = ActionSpecification(name = self.spec_name,
+                                        preconditions = self.preconditions)
 
     def tearDown(self):
         """Gets called after every test case."""
-
-        print("Cleaning up after latest test ...")
 
         del self.spec
 
     def test_input_to_object(self):
 
         self.assertEqual(self.spec_name, self.spec.spec_name)
+        self.assertEqual(self.preconditions, self.spec.preconditions)
+
+
+class ActionHandlingTests(unittest.TestCase):
+    """The calls to the method(s) handling new actions."""
+
+    def setUp(self):
+        """Gets called before every test case."""
+
+        self.spec_name = 'test'
+        self.preconditions = {'a': ['a1', 'a2'],
+                              'b': ['b1']}
+
+        self.spec = ActionSpecification(name = self.spec_name,
+                                        preconditions = self.preconditions)
+
+    def tearDown(self):
+        """Gets called after every test case."""
+
+        del self.spec
+
+    def test_formulas_from_preconditions(self):
+        
+        formula = self.spec._gen_preconditions_formula('a',
+                                                       self.preconditions['a'],
+                                                       act_out = True)
+
+        self.assertIsInstance(formula, PreconditionsFormula)
+
+    def test_not_act_out_raises_exception(self):
+
+        self.assertRaises(NotImplementedError,
+                          self.spec._gen_preconditions_formula,
+                          '', [], False)
+
+    def test_handle_new_action(self):
+
+        self.spec.handle_new_action('b')
+
+        expected_formula = '! b1 -> ! b'
+
+        self.assertEqual(expected_formula, self.spec.sys_trans[0]) # preconditions
+        # self.assertNotEqual(list(), self.spec.env_trans) # activation
+        # self.assertNotEqual(list(), self.spec.env_liveness) # fairness
+
 
 class ConfigurationTests(unittest.TestCase):
-    """Test the generation of Activation-Outcomes formulas"""
+    """Test the construction of the RobotConfiguration class."""
 
     def setUp(self):
         """Gets called before every test case."""
@@ -37,12 +80,8 @@ class ConfigurationTests(unittest.TestCase):
 
         self.config = RobotConfiguration(robot = self.robot)
 
-        print("Setting up a new robot configuration test.")
-
     def tearDown(self):
         """Gets called after every test case."""
-
-        print("Cleaning up after latest test ...")
 
         del self.config
 
