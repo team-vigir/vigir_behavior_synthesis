8464d825026cc27942b0e223b26339acd4271db8
Spyros Maniatopoulos <sm2296@cornell.edu>
2015-06-26 10:13:53 -0400

    [vigir_ltl_specification] Rename task to goal specification

--- vigir_ltl_specification/src/vigir_ltl_specification/goal_specification.py
+++ vigir_ltl_specification/src/vigir_ltl_specification/goal_specification.py
@@ -11,13 +11,12 @@ from gr1_formulas import GR1Formula, FastSlowFormula
 Module's docstring #TODO
 """
 
-VIGIR_ROOT_DIR = os.environ['VIGIR_ROOT_DIR']
 
-class TaskSpecification(GR1Specification):
+class GoalSpecification(GR1Specification):
 	"""..."""
 	
 	def __init__(self, spec_name = '', env_props = [], sys_props = []):
-		super(TaskSpecification, self).__init__(spec_name, env_props, sys_props)
+		super(GoalSpecification, self).__init__(spec_name, env_props, sys_props)
 
 # =========================================================
 # Entry point
@@ -25,11 +24,10 @@ class TaskSpecification(GR1Specification):
 
 def main(): #pragma: no cover
 	
-	my_spec = TaskSpecification()
+	my_spec = GoalSpecification()
 
 	print 'Environment props:\t', my_spec.env_props
 	print 'System props:\t\t', my_spec.sys_props
 
 if __name__ == "__main__": #pragma: no cover
 	main()
-	
